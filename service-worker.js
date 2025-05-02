340258db7990319043110b45347bcbdd67be2a82
Joe Romeo <45112027+JoeFromDenver@users.noreply.github.com>
2025-05-02 10:07:51 -0600

    Update service-worker.js
    Two icons weren't listed before

--- service-worker.js
+++ service-worker.js
@@ -9,12 +9,14 @@ const urlsToCache = [
   // Add paths to your CSS, JS libraries if they were separate files
   // (Currently, your CSS and JS are inline, so only HTML is needed)
   // Add paths to your icons once you create them:
+  'icons/icon-48x48.png',
   'icons/icon-72x72.png',
   'icons/icon-96x96.png',
   'icons/icon-128x128.png',
   'icons/icon-144x144.png',
   'icons/icon-152x152.png',
   'icons/icon-192x192.png',
+  'icons/icon-256x256.png',  
   'icons/icon-384x384.png',
   'icons/icon-512x512.png',
   // Add external libraries if needed (Tone.js is loaded via CDN, might not cache reliably this way)
